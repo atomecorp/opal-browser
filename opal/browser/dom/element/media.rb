dae2164969370096df134094309ec550c6ff844a
Jean-Eric Godard <jeezs@jeezs.net>
2022-10-25 15:54:58 +0200

    Added pause, currentTime,currentTime= to media.rb

--- opal/browser/dom/element/media.rb
+++ opal/browser/dom/element/media.rb
@@ -1,17 +1,35 @@
-module Browser; module DOM; class Element < Node
+module Browser
 
-class Media < Element
-  def play
-    `#@native.play()`
+  module DOM
+
+    class Element < Node
+
+      class Media < Element
+        def play
+          `#@native.play()`
+        end
+
+        def pause
+          `#@native.pause()`
+        end
+
+        def currentTime= (val)
+          `#@native.currentTime = #{val};`
+        end
+
+        def currentTime
+          `#@native.currentTime`
+        end
+      end
+
+      class Video < Media
+        def_selector "video"
+      end
+
+      class Audio < Media
+        def_selector "audio"
+      end
+
+    end
   end
 end
-
-class Video < Media
-  def_selector "video"
-end
-
-class Audio < Media
-  def_selector "audio"
-end
-
-end; end; end
